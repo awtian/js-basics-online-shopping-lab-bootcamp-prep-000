05875f6b32e2bd6d640fee04df83f21c641486d4
Awtian Akbar <me@awtian.com>
2017-11-03 07:09:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var totalPrice = 0
+  for (var i = 0; i < cart.length; i++){
+    var itemNPrice = cart[i]
+    var item = Object.keys(itemNPrice)[0]
+    var price = itemNPrice[item]
+    totalPrice += price
+  }
+return totalPrice
+}
 }
 
 function removeFromCart(item) {
