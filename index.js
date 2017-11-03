a897514aef9ece2025ed743fd163c17967f2f930
Awtian Akbar <me@awtian.com>
2017-11-03 06:56:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var cart = [];
+var itemAndPrices = [];
+for (var i=0; i < cart.length; i++) {
+var itemAndPrice = cart[i]
+var itemn = Object.keys(itemAndPrice)
+var price = cart[item]
+itemAndPrices.push(`${itemn} at /$${price}`)
+}
 
 function getCart() {
  return cart;
@@ -20,8 +27,13 @@ return cart
 
 function viewCart() {
   // write your code here
+  
   if (cart.length===0) {
     console.log(`Your shopping cart is empty.`)
+  }
+  else if (cart.length===1) {
+    console.log (`You have ${itemn} at /$${price}.`)
+  }
   }
 }
 
