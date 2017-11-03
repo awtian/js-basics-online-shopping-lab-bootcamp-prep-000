6f5f2ec3565b2ca96f029345ead6fa833b993f17
Awtian Akbar <me@awtian.com>
2017-11-03 07:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function total() {
 
 function removeFromCart(itemn) {
   // write your code here
-  for (var i = 0; i < cart.length; i++){
+  for (var i = 0; i < cart.length; i++) {
     if(cart[i].hasOwnProperty(itemn)) {
       cart.splice(i,1)
       return cart
