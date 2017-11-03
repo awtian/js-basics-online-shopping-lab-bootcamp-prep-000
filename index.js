dd26fe3ce0160903318037db0da0da4d4dc525af
Awtian Akbar <me@awtian.com>
2017-11-03 06:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     console.log(`Your shopping cart is empty.`)
   }
   else if (cart.length===1) {
-    console.log (`You have ${item} at /$${price}.`)
+    console.log (`You have ${itemn} at /$${price}.`)
   }
 }
 
