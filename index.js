a35be71e41c0dee5335e62f69c1366b47a9ca75e
Awtian Akbar <me@awtian.com>
2017-11-03 07:24:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,4 +70,13 @@ function removeFromCart(itemn) {
 
 function placeOrder(cardNumber) {
   // write your code here
-}
+  if(!cardNumber) {
+     console.log("Sorry, we don't have a credit card on file for you.");
+   }
+   else {
+     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+     for(var i=cart.length;i>0;i--) {
+       cart.pop();
+     }
+   }
+ }
